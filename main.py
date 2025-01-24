a9e4838782758c3f2036ac8991ed047524b8d8fc
albanac808 <alex.alba808+github@gmail.com>
2025-01-23 16:40:58 -1000

    Corrections!

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-social_bookmarks()
+import tkinter as tk
+import webbrowser
+
+def linkedin(event):
+    webbrowser.open('https://www.linkedin.com/in/alexalba/')
+
+def facebook(event):
+    webbrowser.open('https://www.facebook.com/alex.alba')
+
+window=tk.Tk()
+window.geometry("300x200")
+window.title("Social Media Bookmark App")
+
+label1=tk.Label(text="My Social Media", font=("Times new roman",20))
+label1.grid(column=0,row=0)
+
+button1=tk.Button(window,text="Linkedin",bg="orange")
+button1.grid(column=1,row=1)
+button2=tk.Button(window,text="Facebook",bg="pink")
+button2.grid(column=3,row=1)
+button1.bind("<Button-1>",linkedin)
+button2.bind("<Button-1>",facebook)
+
+window.mainloop()
