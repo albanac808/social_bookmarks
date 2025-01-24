23cb85098219f34a16e52c3da43fd4bcafaf71bf
albanac808 <alex.alba808+github@gmail.com>
2025-01-23 15:46:33 -1000

    Social Bookmarks v1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 This is my test workspace for Windows applications in Python.
 
 Project 1 Reference URL found here: https://pythonistaplanet.com/tkinter/
+
+If this error appears: ModuleNotFoundError: No module named 'tkinter'
+Then use these CLI commands:
+    For Ubuntu or other distros with Apt: sudo apt-get install python3-tk
+    For Fedora: sudo dnf install python3-tkinter
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import tkinter as tk
-window = tk.Tk()
-window.title("My first GUI")
+import webbrowser
+
+def linkedin(event):
+    webbrowser.open_new_tab('https://www.linkedin.com/in/alexalba/')
+
+def facebook(event):
+    webbrowser.open_new_tab('https://www.facebook.com/alex.alba')
+
+window=tk.Tk()
 window.geometry("300x200")
+window.title("Social Media Bookmark App")
+
+label1=tk.Label(text="My Social Media", font=("Times new roman",20))
+label1.grid(column=0,row=0)
+
+button1=tk.Button(window,text="Linkedin",bg="orange")
+button1.grid(column=1,row=1)
+button2=tk.Button(window,text="Facebook",bg="pink")
+button2.grid(column=3,row=1)
+button1.bind("<Button-1>",linkedin)
+button2.bind("<Button-1>",facebook)
+
 window.mainloop()
